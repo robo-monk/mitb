b867a83e6aa55e89556f572db4eeaaa635c293ba
robo-monk <rrobomonk@gmail.com>
2022-01-11 02:23:29 +0200

    working

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const captcha = require('./lib/captcha.js');
+// const env = require('./lib/env.js');
 let requestHandler = null;
 
 const fastify = require('fastify')({
@@ -13,19 +15,23 @@ fastify.addContentTypeParser('application/json', {}, (req, body, done) => {
 	done(null, body.body);
 });
 
-fastify.get('/', async (req) => {
-	console.log('yo');
-	return "What's up bitch";
+
+const existingTokens = new Set();
+fastify.post('/request', async ({ body }) => {
+	let token = body.token;
+
+	let pass =
+		!existingTokens.has(token) && (await captcha.validateToken(token));
+
+	const res = pass ? body : { error: 'Invalid' };
+
+	if (existingTokens.size > 1000) existingTokens = new Set();
+	existingTokens.add(token)
+
+	return res;
 });
 
-fastify.post('/postToMeWithJSON', async (req) => {
-	return req.body;
-});
-
-exports.api = functions
-    .region('europe-west1')
-	.https
-	.onRequest((req, res) => {
+exports.api = functions.region('europe-west1').https.onRequest((req, res) => {
 	fastify.ready((err) => {
 		if (err) throw err;
 		requestHandler(req, res);
